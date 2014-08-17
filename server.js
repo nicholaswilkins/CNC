1d9a78a403affe9012b3b70b469577f0ddfb9afd
nicholaswilkins <nicholaspwilkins@gmail.com>
2014-08-17 21:56:55 +0000

    just an update

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ var files=require("./fileServer");
 files.makeServer().listen(8080);
 var termServer = require ("./terminalServer.js");
 termServer.createServer().listen(5000);
+var tty = require('tty.js');
+
+var app = tty.createServer({
+  shell: 'bash',
+  users: {
+    nicbit:"pass"
+  },
+  port: 800
+});
+
+app.get('/', function(req, res, next) {
+  res.send('bar');
+});
+
+app.listen();
